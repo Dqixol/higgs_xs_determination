90ab9d08ab3d02844d29b8615ce0fd8225c25669
Qichen Dong <dong.qichen@cern.ch>
2026-03-30 11:26:45 +0100

    hwt impl

--- libPhys.h
+++ libPhys.h
@@ -1,9 +1,15 @@
 #pragma once
 
-#ifndef __CLING__
-#include <xAOD/TruthParticleContainer.h>
 #include <TLorentzVector.h>
-#endif
+#include <TruthWeightTools/IHiggsWeightTool.h>
+#include <TruthWeightTools/HiggsWeightTool.h>
+#include <TruthWeightTools/HiggsWeights.h>
+
+// namespace TruthWeightTools {
+//     class HiggsWeights;
+//     class IHiggsWeightTool;
+//     class HiggsWeightTool;
+// }
 
 TLorentzVector getTruthHiggsP4(const xAOD::TruthParticleContainer& particles){
     for (auto particle : particles){
@@ -38,4 +44,29 @@ int printTruthParticles(const xAOD::TruthParticleContainer& particles){
     auto photons = getTruthPhotonsP4(particles);
     std::cout << "    pT_yy: " << (photons[0] + photons[1]).Pt() / 1000. << std::endl;
     return count;
+}
+
+TruthWeightTools::HiggsWeights getHiggsWeights(const xAOD::EventInfo& eventInfo, TruthWeightTools::HiggsWeightTool* tool){
+    int HTXS_Njets30        = eventInfo.auxdata<int>("HTXS_Njets_pTjet30");
+    int HTXS_Stage1         = eventInfo.auxdata<int>("HTXS_Stage1_Category_pTjet30");
+    double HTXS_pTH         = eventInfo.auxdata<float>("HTXS_Higgs_pt"); // Needs to be in MeV
+    int HTXS_Stage1p2       = eventInfo.auxdata<int>("HTXS_Stage1_2_Category_pTjet30");
+    int HTXS_Stage1p2Fine   = eventInfo.auxdata<int>("HTXS_Stage1_2_Fine_Category_pTjet30");
+
+    // Access all Higgs weights
+    TruthWeightTools::HiggsWeights hw = tool->getHiggsWeights(
+        HTXS_Njets30, 
+        HTXS_pTH, 
+        HTXS_Stage1, 
+        HTXS_Stage1p2, 
+        HTXS_Stage1p2Fine, 
+        &eventInfo
+    );
+
+    // hw.nominal -> nominal MC weight
+    // hw.pdf4lhc_unc -> 30 Eigen variation for PHD4LHC
+    // hw.alphaS_up -> up alpha_s variaton for PHD4LHC
+    // hw.alphaS_down -> down alpha_s variaton for PHD4LHC
+    // hw.qcd -> muR/muF variation for the given MC
+    return hw;
 }
--- ntupler.py
+++ ntupler.py
@@ -16,50 +16,59 @@ from xAODDataSource import Helpers
 print("Done setting up libraries")
 
 channels = ["mc20e_ggf_hyy", "mc23e_vbf_hyy", "mc20e_vbf_hyy", "mc23e_ggf_hyy"]
-channels = [channel for channel in channels if 'vbf_hyy' in channel]
+channels = [channel for channel in channels if 'mc20e_vbf_hyy' in channel]
+
+weights_to_save = [
+    "hw_nominal",     #   -> nominal MC weight
+    "hw_pdf4lhc_unc", #   -> 30 Eigen variation for PHD4LHC
+    "hw_alphaS_up",   #   -> up alpha_s variaton for PHD4LHC
+    "hw_alphaS_dn", #   -> down alpha_s variaton for PHD4LHC
+    "hw_qcd",         #   -> muR/muF variation for the given MC
+]
 
 # for wrights used see: https://gitlab.cern.ch/atlas_higgs_combination/software/TruthWeightTools/blob/master/Root/HiggsWeightTool.cxx#L131
-weight_signatures = {
-    "mc20e_ggf_hyy": "nnlo",
-    "mc23e_ggf_hyy": "nnlo",
-    "mc23e_vbf_hyy": "nominal",
-    "mc20e_vbf_hyy": "90400",
-}
 
 for channel in channels:
     print(f"Processing {channel}...")
     if channel == "mc20e_ggf_hyy":
-        f = glob.glob("mc20_13TeV.343981.PowhegPythia8EvtGen_NNLOPS_nnlo_30_ggH125_gamgam.deriv.DAOD_PHYS.e5607_s3681_r13145_p7018/*.root.1")
+        f = glob.glob("data/mc20_13TeV.343981.PowhegPythia8EvtGen_NNLOPS_nnlo_30_ggH125_gamgam.deriv.DAOD_PHYS.e5607_s3681_r13145_p7018/*.root.1")
     elif channel == "mc23e_vbf_hyy":
-        f = glob.glob("mc23_13p6TeV.601482.PhPy8EG_PDF4LHC21_VBFH125_gammagamma.deriv.DAOD_PHYS.e8559_s4369_r16083_p7017/*.root.1")
+        f = glob.glob("data/mc23_13p6TeV.601482.PhPy8EG_PDF4LHC21_VBFH125_gammagamma.deriv.DAOD_PHYS.e8559_s4369_r16083_p7017/*.root.1")
     elif channel == "mc20e_vbf_hyy":
-        f = glob.glob("mc20_13TeV.346214.PowhegPy8EG_NNPDF30_AZNLOCTEQ6L1_VBFH125_gamgam.deriv.DAOD_PHYS.e6970_s3681_r13145_p7018/*.root.1")
+        f = glob.glob("data/mc20_13TeV.346214.PowhegPy8EG_NNPDF30_AZNLOCTEQ6L1_VBFH125_gamgam.deriv.DAOD_PHYS.e6970_s3681_r13145_p7018/*.root.1")
     elif channel == "mc23e_ggf_hyy":
-        f = glob.glob("mc23_13p6TeV.602421.PhPy8EG_PDF4LHC21_ggH_NNLOPS_gammagamma.deriv.DAOD_PHYS.e8559_s4369_r16083_p7017/*.root.1")
+        f = glob.glob("data/mc23_13p6TeV.602421.PhPy8EG_PDF4LHC21_ggH_NNLOPS_gammagamma.deriv.DAOD_PHYS.e8559_s4369_r16083_p7017/*.root.1")
     df = Helpers.MakexAODDataFrame(f)
 
-    twt = ROOT.PMGTools.PMGTruthWeightTool(f'truth_weight_tool_{channel}')
-    twt.initialize()
-    weight_names = [str(weight_name) for weight_name in twt.getWeightNames() if weight_signatures[channel] in weight_name]
-    column_names = [weight_name.strip().replace('-', '_').replace(' ', '_').replace('.', 'p').replace(':', '_').replace('=', '_').replace(',', '_') for weight_name in weight_names]
-    print("Saving weights:")
-    print(weight_names)
-
-    twt_ptr = ROOT.addressof(twt)
-
-    df = df.Define('twt', f'''
-        auto ret = reinterpret_cast<PMGTools::PMGTruthWeightTool*>({twt_ptr});
+    hwt = ROOT.TruthWeightTools.HiggsWeightTool( f"HiggsWeightTool_{channel}" )
+    if 'vbf' in channel or 'VH-Had' in channel:
+        hwt.setProperty( "ProdMode", "qq2H"  )
+    elif 'ggf' in channel:
+        hwt.setProperty( "RequireFinite", True )
+        hwt.setProperty( "WeightCutOff", 100.0 )
+        hwt.setProperty( "ProdMode", "ggF" )
+    elif 'WpH' in channel or 'WmH' in channel:
+        hwt.setProperty( "ProdMode", "WH" )
+    elif 'qqZH' in channel:
+        hwt.setProperty( "ProdMode", "qqZH" )
+    elif 'ggZH' in channel:
+        hwt.setProperty( "ProdMode", "ggZH" )
+    elif 'ttH' in channel:
+        hwt.setProperty( "ProdMode", "ttH" )
+    hwt.initialize()
+    hwt_ptr = ROOT.addressof(hwt)
+    df = df.Define('hwt', f'''
+        auto ret = reinterpret_cast<TruthWeightTools::HiggsWeightTool*>({hwt_ptr});
         return ret;
     ''')
 
-    df = df.Filter("TruthBosonsWithDecayParticles.size() >= 3", "TruthBosonsWithDecayParticles.size() >= 3")
-    for weight_name, column_name in zip(weight_names, column_names):
-        df = df.Define(column_name, f'twt->getWeight(&EventInfo, "{weight_name}")')
-
+    df = df.Define("hw", "getHiggsWeights(EventInfo, hwt)")
     df = df.Filter("TruthBosonsWithDecayParticles.size() >= 3", "TruthBosonsWithDecayParticles.size() >= 3")
     df = df.Define("Higgs_p4", "getTruthHiggsP4(TruthBosonsWithDecayParticles)")
     df = df.Define("Photons_p4", "getTruthPhotonsP4(TruthBosonsWithDecayParticles)")
     df = df.Define("photon1_p4", "Photons_p4[0]")
     df = df.Define("photon2_p4", "Photons_p4[1]")
-    df.Snapshot(channel, f"{channel}.root", ["Higgs_p4", "photon1_p4", "photon2_p4"] + column_names)
+    for weight_name in weights_to_save:
+        df = df.Define(weight_name, weight_name.replace("hw_", "hw."))
+    df.Snapshot(channel, f"ntuples/{channel}.root", ["Higgs_p4", "photon1_p4", "photon2_p4"] + weights_to_save)
     df.Report().Print()
