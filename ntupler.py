d94c035fc0b11c94ed437918784eec203a0f2a05
Qichen Dong <dong.qichen@cern.ch>
2026-03-31 15:58:02 +0100

    wh

--- ntupler.py
+++ ntupler.py
@@ -1,6 +1,6 @@
 print("Warming up...")
 import ROOT
-ROOT.EnableImplicitMT(16)
+ROOT.EnableImplicitMT(2)
 ROOT.gROOT.SetBatch()
 ROOT.xAOD.Init()
 ROOT.xAOD.EventInfo()
@@ -11,6 +11,12 @@ ROOT.gInterpreter.Declare('#include "libCpp.h"')
 import libPy
 
 import glob
+import argparse
+argparser = argparse.ArgumentParser(description='myNtupler')
+argparser.add_argument('--channel', help='Channel to process')
+
+args = argparser.parse_args()
+
 
 print("Done setting up libraries")
 
@@ -19,7 +25,9 @@ channels += ["mc20_ggf_hyy", "mc23_vbf_hyy", "mc20_vbf_hyy", "mc23_ggf_hyy"]
 channels += ['mc20_qqzh_hyy', 'mc23_qqzh_hyy']
 channels += ['mc20_wmh_hyy', 'mc23_wmh_hyy', 'mc20_wph_hyy', 'mc23_wph_hyy']
 
-channels = [channel for channel in channels if channel in ["mc23_wph_hyy"]]
+assert args.channel in channels, f"Channel {args.channel} is not in the list of available channels."
+
+channel = args.channel
 
 sample_dict = {
     "mc20_ggf_hyy": 'data/mc20_13TeV.343981.PowhegPythia8EvtGen_NNLOPS_nnlo_30_ggH125_gamgam.deriv.DAOD_PHYS.e5607_s3681_r13145_p7018',
@@ -54,21 +62,20 @@ htxs_categories_to_save = {
 
 # for wrights used see: https://gitlab.cern.ch/atlas_higgs_combination/software/TruthWeightTools/blob/master/Root/HiggsWeightTool.cxx#L131
 
-for channel in channels:
-    print(f"!!!!!!!!!!!!!!!!!Processing {channel}...!!!!!!!!!!!!!!!!!!")
-    f = glob.glob(sample_dict[channel] + "/*.root.1")
-    df = Helpers.MakexAODDataFrame(f)
+print(f"!!!!!!!!!!!!!!!!!Processing {channel}...!!!!!!!!!!!!!!!!!!")
+f = glob.glob(sample_dict[channel] + "/*.root.1")
+df = Helpers.MakexAODDataFrame(f)
 
-    df, hwt = libPy.getHiggsWeightTool(df, channel)
+df, hwt = libPy.getHiggsWeightTool(df, channel)
 
-    df = df.Define("hw", "getHiggsWeights(EventInfo, hwt)")
+df = df.Define("hw", "getHiggsWeights(EventInfo, hwt)")
 
-    # df = df.Define("Higgs_p4", "getTruthHiggsP4(TruthBosonsWithDecayParticles)")
-    # df = df.Define("Photons_p4", "getTruthPhotonsP4(TruthBosonsWithDecayParticles)")
-    # df = df.Define("photon1_p4", "Photons_p4[0]")
-    # df = df.Define("photon2_p4", "Photons_p4[1]")
-    for cata, dtype in htxs_categories_to_save.items():
-        df = df.Define(cata, f'EventInfo.auxdataConst<{dtype}>  ("{cata}")')
-    for weight_name in weights_to_save:
-        df = df.Define(weight_name, weight_name.replace("hw_", "hw."))
-    df.Snapshot(channel, f"ntuples/{channel}_stxs.root", weights_to_save + list(htxs_categories_to_save.keys()))
+# df = df.Define("Higgs_p4", "getTruthHiggsP4(TruthBosonsWithDecayParticles)")
+# df = df.Define("Photons_p4", "getTruthPhotonsP4(TruthBosonsWithDecayParticles)")
+# df = df.Define("photon1_p4", "Photons_p4[0]")
+# df = df.Define("photon2_p4", "Photons_p4[1]")
+for cata, dtype in htxs_categories_to_save.items():
+    df = df.Define(cata, f'EventInfo.auxdataConst<{dtype}>  ("{cata}")')
+for weight_name in weights_to_save:
+    df = df.Define(weight_name, weight_name.replace("hw_", "hw."))
+df.Snapshot('tree', f"ntuples/{channel}_stxs.root", weights_to_save + list(htxs_categories_to_save.keys()))
